32e4736a17317a4fd55f1f82a25c24873c9f2d7e
Leo <leodanis.miranda@gmail.com>
2023-07-16 00:41:36 -0400

    ultima

--- src/app/pages/ru-tratamientos/ru-tratamientos.page.ts
+++ src/app/pages/ru-tratamientos/ru-tratamientos.page.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 import Swiper from 'swiper';
 
 @Component({
@@ -25,7 +26,25 @@ export class RuTratamientosPage implements OnInit {
       { tab: 'mesage', icon: '../../../assets/msj.svg' },
       { tab: 'profile', icon: '../../../assets/person.svg' }
     ];
-  constructor() { }
+
+    iconStyles = [ 
+      { tab: 'cart', styles: { color: 'red', fontSize: '24px' } },
+      { tab: 'notification', styles: { color: 'blue', fontSize: '24px', marginTop: '-5px' } },
+      { tab: 'mesage', styles: { color: 'blue', fontSize: '24px',  marginTop: '-5px' } },
+      { tab: 'profile', styles: { color: 'green', fontSize: '24px', marginTop:'-20px' } }
+    ];
+   
+    tabRoutes = [
+      { tab: 'cart', route: '/tratamiento3' },
+      { tab: 'notification', route: '/home' },
+      { tab: 'mesage', route: '/' },
+      { tab: 'profile', route: '/' }
+    ];
+    
+  
+    constructor(public router: Router) { }
+
+
 
   ngOnInit() {
   }
